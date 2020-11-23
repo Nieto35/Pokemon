9661a490e19ebdc422a52e99e64bede0b12f7fe0
Felipe <Nieto>
2020-11-23 14:33:37 -0500

    modal con informacion del pokemon

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,89 @@
 import React from 'react';
+import Detail from './pokemonDetail';
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
-function Card(props){
-    return (
-        <div className="card-container">
-            <h2>{props.name}</h2>
-            <h3>{props.num}</h3>
-            <img src={props.img} alt={props.name} />
-            <button>DETALLE</button>
+export default class Card extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            abierto: false,
+            pokemonDetails: []
+        }
+    }
+    
+    abrirModal=(num)=> {
+        this.setState({abierto: !this.state.abierto});
+        this.infoPokemon(num);
+
+    }
+
+    componentDidMount() {
+
+
+        const url = 'https://pokeapi.co/api/v2/pokemon/1';
+        
+        //Consumir la API de pokeapi
+        fetch(`${url}`)
+            .then(response => response.json())
+            .then(data => this.setState({pokemonDetails: data.stats}))
+            .catch( error => {
+              console.log(error);
+            })
+
+            
+
+    }
+
+
+    infoPokemon = (num) => {
+        //1. Guardar los detalles de un pokemon
+  
+        const url = 'https://pokeapi.co/api/v2/pokemon/';
+
+  
+        fetch(`${url}${num}`)
+  
+              .then(response => response.json())
+  
+              .then(data => this.setState({pokemonDetails: data.stats}))
+  
+              .catch( error => {
+  
+                console.log(error);
+  
+              })
+    
+      }
+
+    render() {
+
+        return (
+            <div className="card-container">
+            <h2>{this.props.name}</h2>
+            <h3>{this.props.num}</h3>
+            <img src={this.props.img} alt={this.props.name} />
+            <Button onClick= {() => this.abrirModal(this.props.num)}> Detalles</Button>
+
+            <Modal isOpen={this.state.abierto}>
+                <ModalHeader>
+                    {this.props.name}
+                </ModalHeader>
+                <ModalBody>
+                    <img src={this.props.img} alt={this.props.name} />
+                    {this.state.pokemonDetails.map((detail, index) => <Detail key= {index} name ={detail.stat.name} stat = {detail.base_stat}/>)}
+        
+                     
+                </ModalBody>
+                <ModalFooter>
+                    <Button onClick= {() => this.abrirModal(this.props.num)}> Cerrar </Button>
+                </ModalFooter>
+             </Modal>
+             
+           
         </div>
-    )
-}
+        );
+    }
 
-export default Card;
+
+}
---
+++ src/components/pokemonDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+
+const Detail = (props) => {
+ 
+    return(
+        <div>
+            <h3>{props.name}</h3>
+
+            <span>{props.stat}</span>
+        </div>
+        
+     );
+    
+};
+
+export default Detail
+
+
