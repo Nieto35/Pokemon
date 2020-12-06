d787c4cf21b8616d273b42b6d0b26d5e2c839236
Nieto <jfnieto00@misena.edu.co>
2020-12-06 11:50:17 -0500

    solucion de errores y warnings

--- src/App.js
+++ src/App.js
@@ -10,23 +10,7 @@ export default class App extends React.Component {
             pokemones: [],
             currentPage: 1,
             pokemonPerPage: 10,
-            pokemonImg :  0,
-            // types: {
-            //   fire: '#FDDFDF',
-            //   grass: '#DEFDE0',
-            //   electric: '#FCF7DE',
-            //   water: '#DEF3FD',
-            //   ground: '#f4e7da',
-            //   rock: '#d5d5d4',
-            //   fairy: '#fceaff',
-            //   poison: '#98d7a5',
-            //   bug: '#f8d5a3',
-            //   dragon: '#97b3e6',
-            //   psychic: '#eaeda1',
-            //   flying: '#F5F5F5',
-            //   fighting: '#E6E0D4',
-            //   normal: '#F5F5F5'
-            // }
+            pokemonImg :  0
         }
     }
 
@@ -55,7 +39,6 @@ export default class App extends React.Component {
 
       const limit = this.state.pokemonPerPage;
       const url = 'https://pokeapi.co/api/v2/pokemon';
-      const type = 
 
 
       fetch(`${url}?limit=${limit}&offset=${(requestPage - 1) * 10}`)
--- src/components/pokemonDetail.js
+++ src/components/pokemonDetail.js
@@ -8,11 +8,11 @@ const Detail = (props) => {
     return(
         <div key={props.index}>
             <span className="span-strong">{
-                props.name == 'hp' ? <span><Icon>favorite</Icon> {props.name}</span> : 
-                props.name == 'attack' ? <span><Icon>flash_on</Icon> {props.name}</span>  : 
-                props.name == 'defense' ? <span><Icon>security</Icon> {props.name}</span>  : 
-                props.name == 'special-attack' ? <span><Icon>local_fire_department</Icon> {props.name}</span> : 
-                props.name == 'special-defense' ? <span><Icon>verified_user</Icon> {props.name}</span> : 
+                props.name === 'hp' ? <span><Icon>favorite</Icon> {props.name}</span> : 
+                props.name === 'attack' ? <span><Icon>flash_on</Icon> {props.name}</span>  : 
+                props.name === 'defense' ? <span><Icon>security</Icon> {props.name}</span>  : 
+                props.name === 'special-attack' ? <span><Icon>local_fire_department</Icon> {props.name}</span> : 
+                props.name === 'special-defense' ? <span><Icon>verified_user</Icon> {props.name}</span> : 
                 <span><Icon>speed</Icon> {props.name}</span> 
              }  : </span>
 
